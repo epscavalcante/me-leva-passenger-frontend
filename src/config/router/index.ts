82cc80f0ba8972bb30a3c4e299caf48a92c4b01a
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2025-02-21 20:18:04 -0400

    add config for app, routes  and store

---
+++ src/config/router/index.ts
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../../views/HomeView.vue'),
+    },
+    {
+      path: '/rides',
+      name: 'rides',
+      component: () => import('../../views/RidesView.vue'),
+    },
+
+    {
+      path: '/account',
+      name: 'account',
+      component: () => import('../../views/AccountView.vue'),
+    },
+  ],
+})
+
+export default router
