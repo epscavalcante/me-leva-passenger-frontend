33eb0ebef0bdae6abfd7d7cdc66095f3412d8d9f
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2025-02-27 15:35:28 -0400

    configure pusher and laravel echo

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,25 @@ import RideService from './services/RideService'
 import FetchHttpAdapter from './clients/http/FetchHttpClient'
 import Config from './config/app'
 import { RideHttpGateway } from './gateways/RideGateway'
+import Echo from 'laravel-echo'
+import Pusher from 'pusher-js'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+window.Pusher = Pusher
+
+window.Echo = new Echo({
+  broadcaster: 'reverb',
+  key: 'my-app-key',
+  wsHost: 'localhost',
+  wsPort: 9000,
+  //wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+  forceTLS: false,
+  enabledTransports: ['ws', 'wss'],
+})
 
 const fetchHttpAdapter = new FetchHttpAdapter(Config.apiUrl)
 
