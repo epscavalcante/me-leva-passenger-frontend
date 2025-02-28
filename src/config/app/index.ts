12e5f01ec41334a938fa44f1db69f16929342f6d
Eduardo Cavalcante <eduardo.ps.cavalcante@gmail.com>
2025-02-28 08:14:33 -0400

    set default api base url

--- src/config/app/index.ts
+++ src/config/app/index.ts
@@ -4,6 +4,6 @@ export default class Config {
   }
 
   static get apiUrl() {
-    return import.meta.env.VITE_API_BASE_URL
+    return import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000'
   }
 }
